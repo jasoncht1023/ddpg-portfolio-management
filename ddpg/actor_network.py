added uniform weighting eval, fixed model saving

ddpg/actor_network.py
@@ -43,9 +43,9 @@ class ActorNetwork(nn.Module):
         T.save(self.state_dict(), self.checkpoint_file)
 
     def load_checkpoint(self):
-        print("... loading checkpoint ...")
-        self.load_state_dict(T.load(self.checkpoint_file))
-
+        if os.path.exists(self.checkpoint_file): 
+            print("... loading checkpoint ...")
+            self.load_state_dict(T.load(self.checkpoint_file))
 
 # for testing only
 if __name__ == "__main__":

ddpg/agent.py
@@ -1,17 +1,19 @@
 import torch as T
 import torch.nn.functional as F
+import torch.nn as nn
 import numpy as np
 from .ou_action_noise import OUActionNoise
 from .replay_buffer import ReplayBuffer
 from .actor_network import ActorNetwork
 from .critic_network import CriticNetwork
+import os
 
 
 # alpha and beta are the learning rate for actor and critic network, gamma is the discount factor for future reward
 # tau is the "update rate" of the target networks oarameters (param_target = tau * param_online + (1-tau) * param_target)
 class Agent(object):
     def __init__(self, alpha, beta, input_dims, tau, gamma=0.99, 
-                 n_actions=2, max_size=1000000, batch_size=64):
+                 n_actions=2, max_size=100000, batch_size=64):
         self.gamma = gamma
         self.tau = tau
         self.memory = ReplayBuffer(max_size, input_dims, n_actions)
@@ -31,15 +33,17 @@ class Agent(object):
         #                                    layer2_size, n_actions=n_actions,
         #                                    name='TargetCritic')
 
-        self.actor = ActorNetwork(learning_rate=alpha, n_actions=n_actions, name="Actor")
+        self.actor = ActorNetwork(learning_rate=alpha, n_actions=n_actions, name="actor", chkpt_dir="temp")
 
-        self.critic = CriticNetwork(learning_rate=beta, n_actions=n_actions, name="Critic")
+        self.critic = CriticNetwork(learning_rate=beta, n_actions=n_actions, name="critic", chkpt_dir="temp")
 
-        self.target_actor = ActorNetwork(learning_rate=alpha, n_actions=n_actions, name="TargetActor")
+        self.target_actor = ActorNetwork(learning_rate=alpha, n_actions=n_actions, name="target_actor", chkpt_dir="temp")
 
-        self.target_critic = CriticNetwork(learning_rate=beta, n_actions=n_actions, name="TargetCritic")
+        self.target_critic = CriticNetwork(learning_rate=beta, n_actions=n_actions, name="target_critic", chkpt_dir="temp")
 
         self.noise = OUActionNoise(mu=np.zeros(n_actions))
+
+        self.softmax = nn.Softmax(dim=-1)
 
         self.update_network_parameters(tau=1)
 
@@ -49,16 +53,11 @@ class Agent(object):
         mu = self.actor.forward(observation).to(self.actor.device)
         mu_prime = mu + T.tensor(self.noise(), dtype=T.float).to(self.actor.device)
         self.actor.train()
-        # return mu_prime.cpu().detach().numpy()
+        print("mu_prime after adding noise:", mu_prime)
+        mu_prime = self.softmax(mu_prime)               # actions sum to 1
+        mu_prime = mu_prime / mu_prime.sum()       
 
-        # For testing only, uncomment the above return statement and delete the below code
-        mu_prime = mu_prime.cpu().detach().numpy()
-        print("untransformed mu_prime:", mu_prime)
-        mu_prime = np.array(list(map(abs, mu_prime)))
-        print("mu_prime:", mu_prime)
-        mu_prime = mu_prime / mu_prime.sum()            # actions sum to 1
-
-        return mu_prime
+        return mu_prime.cpu().detach().numpy()
 
     def remember(self, old_input_tensor, action, reward, new_input_tensor, done):
         self.memory.store_transition(old_input_tensor, action, reward, new_input_tensor, done)
@@ -86,10 +85,10 @@ class Agent(object):
         # The targets we want to move towards
         # refactor needed
         target_actions = [
-            self.target_actor.forward(input_tensor) for input_tensor in old_input_tensor
+            self.target_actor.forward(input_tensor) for input_tensor in new_input_tensor
         ]
         target_critic_value = [
-            self.target_critic.forward(old_input_tensor[i], action)
+            self.target_critic.forward(new_input_tensor[i], action)
             for i, action in enumerate(target_actions)
         ]
         critic_value = [
@@ -171,6 +170,9 @@ class Agent(object):
         """
 
     def save_models(self):
+        if not os.path.isdir("temp"): 
+            os.makedirs("temp")
+
         self.actor.save_checkpoint()
         self.target_actor.save_checkpoint()
         self.critic.save_checkpoint()

main.py
@@ -5,50 +5,113 @@ import matplotlib.pyplot as plt
 import os
 
 # Configurations
+# Portfolio settings
 assets = [
-    "FUTU",
+    "MMM",
     "NVDA",
+    "GS",
+    "NKE",
+    "AXP",
+    "HD",
+    "PG",
+    "AMGN",
+    "HON"
 ]
 rebalance_window = 10
 tx_fee_per_share = 0.005
-principal=1000000
-num_epoch = 5
+principal = 1000000
+num_epoch = 10
 
-env = TradingSimulator(principal=principal, assets=assets, start_date="2024-01-01", end_date="2024-11-11", 
+# Evaluation settings, 1: mode will be evaluated; 0: mode will not be run
+evaluation_mode = {
+    "ddpg": 1,
+    "uniform_with_rebalance": 1,
+    "uniform_without_rebalance": 1
+}
+
+# Evaluation metrics
+return_history = {}
+sharpe_ratio_history = {}
+
+# Trading environment initialization
+env = TradingSimulator(principal=principal, assets=assets, start_date="2024-01-01", end_date="2024-12-31", 
                        rebalance_window=rebalance_window, tx_fee_per_share=tx_fee_per_share)
 
-# Default alpha=0.000025, beta=0.00025, tau=0.001, batch_size=64
-agent = Agent(alpha=0.000025, beta=0.00025, input_dims=[4, len(assets), rebalance_window, len(assets)],
-              tau=0.001, batch_size=8, n_actions=len(assets)+1)
+if (evaluation_mode["ddpg"] == 1):
+    # Default alpha=0.000025, beta=0.00025, tau=0.001, batch_size=64
+    agent = Agent(alpha=0.000025, beta=0.00025, input_dims=[4, len(assets), rebalance_window, len(assets)],
+                tau=0.001, batch_size=8, n_actions=len(assets)+1)
 
-# agent.load_models()
-np.random.seed(0)
+    agent.load_models()
+    np.random.seed(0)
 
-score_history = []
-sharpe_ratio_history = []
-for i in range(num_epoch):
-    print(f"-----------------Episode {i+1}-----------------")
+    return_history["ddpg"] = []
+    sharpe_ratio_history["ddpg"] = []
+    print("--------------------DDPG--------------------")
+    for i in range(num_epoch):
+        print(f"-----------------Episode {i+1}-----------------")
+        observation = env.restart()
+        done = 0
+        total_return = 0
+        while not done:
+            action = agent.choose_action(observation)
+            print("action:", action)
+            new_state, reward, done = env.step(action)
+            print("reward:", reward)
+            agent.remember(observation, action, reward, new_state, done)
+            agent.learn()
+            total_return += reward
+            observation = new_state
+        return_history["ddpg"].append(total_return)
+        sharpe_ratio = env.sharpe_ratio()
+        sharpe_ratio_history["ddpg"].append(sharpe_ratio)
+
+        if i % 5 == 0:
+            agent.save_models()
+        print(f"------Episode {i+1} Summary: Total Return {total_return:.2f}; Sharpe Ratio {sharpe_ratio:.5f};------")
+
+if (evaluation_mode["uniform_with_rebalance"] == 1):
+    print("--------------------Uniform Weighting with Rebalancing--------------------")
     observation = env.restart()
     done = 0
-    score = 0
+    total_return = 0
     while not done:
-        action = agent.choose_action(observation)
+        action = [1/(len(assets)+1)] * (len(assets)+1)
         print("action:", action)
         new_state, reward, done = env.step(action)
         print("reward:", reward)
-        agent.remember(observation, action, reward, new_state, done)
-        agent.learn()
-        score += reward
-        observation = new_state
-    score_history.append(score)
+        total_return += reward
     sharpe_ratio = env.sharpe_ratio()
-    sharpe_ratio_history.append(sharpe_ratio)
 
-    # if i % 25 == 0:
-    #    agent.save_models()
-    print(f"------Episode {i+1} Summary: Score {score:.2f}; Sharpe Ratio {sharpe_ratio:.5f}; Trailing 100 games avg {np.mean(score_history[-100:]):.3f} ------")
+    return_history["uniform_with_rebalance"]= [total_return] * num_epoch
+    sharpe_ratio_history["uniform_with_rebalance"] = [sharpe_ratio] * num_epoch
 
-# Generating evaluation graphs
+    print(f"------Summary: Total Return {total_return:.2f}; Sharpe Ratio {sharpe_ratio:.5f};------")
+
+if (evaluation_mode["uniform_without_rebalance"] == 1):
+    print("--------------------Uniform Weighting without Rebalancing--------------------")
+    observation = env.restart()
+    done = 0
+    total_return = 0
+    action = [1/(len(assets)+1)] * (len(assets)+1)
+    print("action:", action)
+    new_state, reward, done = env.step(action)
+    print("reward:", reward)
+    total_return += reward
+    while not done:
+        action = []
+        print("action:", action)
+        new_state, reward, done = env.step(action)
+        print("reward:", reward)
+        total_return += reward
+    sharpe_ratio = env.sharpe_ratio()
+
+    return_history["uniform_without_rebalance"]= [total_return] * num_epoch
+    sharpe_ratio_history["uniform_without_rebalance"] = [sharpe_ratio] * num_epoch
+
+    print(f"------Summary: Total Return {total_return:.2f}; Sharpe Ratio {sharpe_ratio:.5f};------")
+
+# Plotting the evaluation graphs
 if not os.path.isdir("evaluation"): 
     os.makedirs("evaluation")
         
@@ -56,14 +119,23 @@ xAxis = range(1, num_epoch+1)
 
 plt.title("Total return over epoch")
 plt.xlabel('Epoch') 
-plt.ylabel('Total return')                  
-plt.plot(xAxis, score_history)
+plt.ylabel('Total return')
+
+for mode in evaluation_mode:
+    if (evaluation_mode[mode] == 1):
+        plt.plot(xAxis, return_history[mode], label=mode)
+
+plt.legend()
 plt.savefig("evaluation/total_return.png", dpi=300, bbox_inches="tight")
 plt.clf()
 
 plt.title("Sharpe Ratio over epoch")
 plt.xlabel('Epoch') 
 plt.ylabel('Sharpe Ratio')                      
-plt.plot(xAxis, sharpe_ratio_history)
-plt.savefig("evaluation/sharpe_ratio.png", dpi=300, bbox_inches="tight")
 
+for mode in evaluation_mode:
+    if (evaluation_mode[mode] == 1):
+        plt.plot(xAxis, sharpe_ratio_history[mode], label=mode)
+
+plt.legend()
+plt.savefig("evaluation/sharpe_ratio.png", dpi=300, bbox_inches="tight")