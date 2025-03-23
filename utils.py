980faaca6d12433f7ad5927db6e2eedf5fa32360
Jason Chan <jasoncht1023@gmail.com>
2025-03-23 23:07:35 +0800

    Added avg monthly return evaluation

--- utils.py
+++ utils.py
@@ -46,3 +46,14 @@ def plot_testing_return(time_axis, testing_modes, return_history):
     plt.legend()
     plt.savefig("evaluation/testing_cumulative_return.png", dpi=300, bbox_inches="tight")
     plt.clf()
+
+def print_eval_results(env, total_return):
+    sharpe_ratio = env.sharpe_ratio()
+    omega_ratio = env.omega_ratio(15)
+    mdd = env.maximum_drawdown()
+    portfolio_value = env.total_portfolio_value()
+    avg_yearly_return = env.avg_yearly_return()
+    avg_monthly_return = env.avg_monthly_return()
+    print(f"------Portfolio Value {portfolio_value:.2f}; Total Return {total_return:.2f};------")
+    print(f"------Sharpe Ratio {sharpe_ratio:.5f}; Omega Ratio {omega_ratio:.5f} MDD {mdd:.5f}------")
+    print(f"------Average yearly return {avg_yearly_return:.5f}%; Average monthly return {avg_monthly_return:.5f}%------\n")
